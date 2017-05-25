d0dae9e8ff3d4f5762e32e05c32c5176318234d0
Matt Weldon <matt@mattweldon.com>
2017-05-25 22:23:28 +0100

    Feature (content): Add content component

---
+++ addon/components/indie-content.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import layout from '../templates/components/indie-app';
+
+const {
+  Component,
+} = Ember;
+
+export default Component.extend({
+  // Attributes
+  layout,
+  tagName: 'main',
+  classNames: [
+    'w-90',
+    'w-70-l',
+    'pa3',
+    'ph4-ns',
+    'center'
+  ]
+});
