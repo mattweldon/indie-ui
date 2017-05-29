a4069c793d7d91eff466277d89b3b9c96d0b52b7
Matt Weldon <matt@mattweldon.com>
2017-05-29 10:58:16 +0100

    Feature (buttons): Add button component logic

--- addon/components/indie-button.js
+++ addon/components/indie-button.js
@@ -9,8 +9,52 @@ export default Component.extend({
   // Attributes
   layout,
   tagName: 'button',
+  classNames: [
+    'ba',
+    'bw0',
+    'ml3',
+    'br1',
+    'pa3',
+    'ph4',
+    'f6',
+    'pointer',
+    'bg-animate',
+  ],
+  primaryClassNames: [
+    'hover-bg-dark-blue',
+    'bg-blue',
+    'b--dark-blue',
+    'white',
+  ],
+  positiveClassNames: [
+    'hover-bg-dark-green',
+    'bg-green',
+    'b--dark-green',
+    'white',
+  ],
+  negativeClassNames: [
+    'hover-bg-dark-red',
+    'bg-red',
+    'b--dark-red',
+    'white',
+  ],
+  linkClassNames: [
+    'blue',
+    'link',
+    'underline',
+    'bg-white'
+  ],
+  // Methods
+  didReceiveAttrs() {
+    var styleName = this.get('style');
+    var styleClasses = this.get(styleName + 'ClassNames');
+    var defaultClasses = this.get('classNames');
+    this.set('classNames', defaultClasses.concat(styleClasses));
+  },
   click() {
-    this.sendAction('action', this);
-    this.sendAction('on-click', this);
+    if (!this.get('disabled')) {
+      this.sendAction('action', this);
+      this.sendAction('on-click', this);
+    }
   }
 });
